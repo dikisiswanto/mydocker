ff64c32b30704217bdfbebd773d3d6dfda4e8a06
Diki Siswanto <dickyachmady@gmail.com>
2019-05-18 06:36:45 +0800

    Perubahan tampilan halaman dari image docker

--- src/index.php
+++ src/index.php
@@ -1,13 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Hello, Docker!</title>
-</head>
-<body>
-	<h1><?php echo 'Hello docker!' ?></h1>
-	<h2>I am Diki Siswanto</h2>
-</body>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Diki Siswanto</title>
+    <meta name="theme-color" content="#00d1b2">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.7.2/css/bulma.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.7/css/all.css">
+  </head>
+  <body>
+  <section class="hero is-fullheight is-primary">
+    <div class="hero-body has-text-center">
+      <div class="container">
+      	<h1 class="title">Hello, World!</h1>
+	      <p class="subtitle">
+	        I'm <strong>Diki Siswanto</strong> from <strong>Makassar, ID.</strong>
+	      </p>
+	      <div class="social-links">
+	      	<a href="http://facebook.com/dicky.achmdy" class="icon button is-medium is-primary has-text-light" target="_blank" rel="nofollow">
+	      		<i class="fab fa-facebook fa-lg"></i>
+	      	</a>
+	      	<a href="http://github.com/dikisiswanto" class="icon button is-medium is-primary has-text-light" target="_blank" rel="nofollow">
+	      		<i class="fab fa-github fa-lg"></i>
+	      	</a>
+	      	<a href="http://medium.com/@dikisiswanto" class="icon button is-medium is-primary has-text-light" target="_blank" rel="nofollow">
+	      		<i class="fab fa-medium fa-lg"></i>
+	      	</a>
+	      </div>
+      </div>
+    </div>
+  </section>
+  </body>
 </html>
